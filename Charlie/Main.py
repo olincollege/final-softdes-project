81226a603c6953117fab9d180523bd0857215f55
Charlie Babe <cbabe3909@gmail.com>
2021-04-30 15:45:54 -0400

    Changed function call

--- Charlie/Main.py
+++ Charlie/Main.py
@@ -5,8 +5,6 @@ def main():
     
     model = PhotoModel
     photo=PhotoView(model)
-    #img_path = photo.img_path()
-    #model.open_img(img_path)
     photo.GUI()
 if __name__ == "__main__":
     main()
